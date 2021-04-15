d7ae3e063624639407ca51546baf0581dfc75bcb
Rahul Devadiga <Devadiga.Rahul@in.bosch.com>
2021-04-15 11:45:13 +0530

    commit

--- typewise_alert.py
+++ typewise_alert.py
@@ -1,4 +1,4 @@
-individual_temperature_range={
+coolingType_temperature_range={
   'PASSIVE_COOLING':{'lowerLimit':0, 'upperLimit':35},
   'MED_ACTIVE_COOLING':{'lowerLimit':0, 'upperLimit':40},
   'HI_ACTIVE_COOLING':{'lowerLimit':0, 'upperLimit':45}
@@ -12,23 +12,32 @@ def infer_breach(value, lowerLimit, upperLimit):
   return 'NORMAL'
 
 def classify_temperature_breach(coolingType, temperatureInC):
-  temperature_range=individual_temperature_range[coolingType]
-  return infer_breach(temperatureInC, temperature_range['lowerLimit'], temperature_range['upperLimit'])
+  temperature_range=coolingType_temperature_range.get(coolingType)
+  if(temperature_range!=None):
+    return infer_breach(temperatureInC, temperature_range['lowerLimit'], temperature_range['upperLimit'])
+
 
 def check_and_alert(alertTarget, batteryChar, temperatureInC):
   breachType =classify_temperature_breach(batteryChar['coolingType'], temperatureInC)
-  message_target[alertTarget](breachType)
+  if(breachType!=None):
+    #message_to_target[alertTarget](breachType)
+    return message_to_target[alertTarget](breachType) #Mocking and validation purpose
 
-def send_to_console(breachType):
+def send_to_console(breachType):#mocking console and verify Input received to console
   header = 'Console:'
   print(f'{header}, {breachType}')
+  return breachType
 
-def send_to_controller(breachType):
+def send_to_controller(breachType):#mocking console and verify Input received to controller
   header = 0xfeed
   print(f'{header}, {breachType}')
-  
-   
-def send_to_email(breachType):
+  return breachType
+
+def send_to_email(breachType):#mocking console and verify Input received to email
+  frame_mail_to_print(breachType)
+  return breachType
+
+def frame_mail_to_print(breachType):
   recepient = "a.b@c.com"
   text_msg={
     'TOO_LOW':'too low',
@@ -40,7 +49,7 @@ def send_to_email(breachType):
       print(f'Hi, the temperature is {message}')
   return print_mail(text_msg.get(breachType))
 
-message_target={
+message_to_target={
   'to_console':send_to_console,
   'to_controller':send_to_controller,
   'to_email':send_to_email
